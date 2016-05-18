3beba7adf0c0e9467377fef589abaacbe3e158f6
Eugene Viktorov <eugene.viktorov@anvileight.com>
2016-05-18 23:13:46 +0300

    Implement Zip Bomb checker

--- zip_test.py
+++ zip_test.py
@@ -2,16 +2,26 @@ import os
 import zipfile
 
 
-def search_zip_for_png(zip_file_name):
-    zf = zipfile.ZipFile(zip_file_name, 'r')
-    for name in zf.namelist():
-        if name[-4:] == '.png':
-            print('"%s" - found PNG in "%s"' % (name.split('/')[-1],
-                                                zip_file_name))
-            return zf
-    print('Not found PNG in "%s"' % zip_file_name)
-    return False
+def get_zips_png_file_names(zip_object):
+    return [name for name in zip_object.namelist() if name[-4:] == '.png']
+
+
+def check_zip_names_on_archive_bomb(zip_object, names_to_check):
+    total_size_diff = 0
+    for info in (info for info in zip_object.infolist()
+                 if info. filename in names_to_check):
+        total_size_diff += info.file_size - info.compress_size
+    print('\tTotal size diff:\t', total_size_diff, 'bytes')
+    if total_size_diff > 10 ** 9:
+        raise MemoryError
+
 
 if __name__ == "__main__":
-    search_zip_for_png('python_html.zip')
-    search_zip_for_png('python_html_no_png.zip')
+    # search_zip_for_png('python_html.zip')
+    # zf = zipfile.ZipFile('python_html_no_png.zip', 'r')
+    # zf = zipfile.ZipFile('python_html.zip', 'r')
+    zf = zipfile.ZipFile('png_html_page.zip', 'r')
+    png_files = get_zips_png_file_names(zf)
+    # print(png_files)
+    print(check_zip_names_on_archive_bomb(zf, png_files))
+    # get_zip_infolist('python_html_no_png.zip')
