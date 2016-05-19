22880c62dcf17a3faece0f4cfc949ae63079fc23
Eugene Viktorov <eugene.viktorov@anvileight.com>
2016-05-19 16:09:43 +0300

    Implement PNG compression

--- copy_archive.py
+++ copy_archive.py
@@ -1,5 +1,4 @@
 import os
-from os.path import join
 import zipfile
 import tempfile
 
@@ -40,10 +39,21 @@ def copy_archive(zipfile_name):
                 zf_copy_source.namelist()
             )
             os.chdir(tmpdirname)
-            names_to_copy = zf_copy_source.namelist()
 
+            with os.popen(
+                    'find . -name "*.png" -exec '
+                    'pngquant --ext ".png" --force 16 {} \;') as file_:
+                console_run_result = file_.read()
+
+            if console_run_result:
+                raise Exception('Failed to perform PNG compression')
+
+            names_to_copy = zf_copy_source.namelist()
             for name in names_to_copy:
                     zf_copy_to.write(name, compress_type=compression)
+
+    except Exception:
+        os.remove(zf_copy_to.filename)
 
     finally:
         os.chdir(curr_cwd)
@@ -53,15 +63,16 @@ def copy_archive(zipfile_name):
     if os.path.exists(zf_copy_to.filename):
         print('Done => "%s"' % zf_copy_to.filename)
         print('--------------')
-        print_info(zf_copy_to.filename)
-        print('--------------')
+        # print_info(zf_copy_to.filename)
+        # print('--------------')
+
         # TODO: Delete cleanup
-        print("Do cleanup...")
-        os.remove(zf_copy_to.filename)
+        # print("Do cleanup...")
+        # os.remove(zf_copy_to.filename)
     else:
         raise FileExistsError("Failed to copy archive")
 
 
 if __name__ == "__main__":
-    name_to_copy = create_archive()
+    name_to_copy = 'png_html_page.zip'
     copy_archive(name_to_copy)
