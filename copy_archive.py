86db352bcef73a7d7c86d8cea410d9733484c71d
Eugene Viktorov <eugene.viktorov@anvileight.com>
2016-05-19 14:29:14 +0300

    Implement extracting archive to the temporarry dir

--- copy_archive.py
+++ copy_archive.py
@@ -1,4 +1,7 @@
+import os
 import zipfile
+import tempfile
+
 try:
     import zlib
     compression = zipfile.ZIP_DEFLATED
@@ -10,27 +13,32 @@ from zip_infolist import print_info
 
 def create_archive():
     print("----------\nCreate archive")
-    zf = zipfile.ZipFile("zipfile_append.zip", mode='w')
+    zf = zipfile.ZipFile("tmp.zip", mode='w')
     try:
         zf.write('random_text.txt')
         zf.write('random_text.txt', arcname='random_text_compressed.txt',
                  compress_type=compression)
     finally:
         zf.close()
-    print()
-    print_info('zipfile_append.zip - DONE!\n---------------')
+    print_info(zf.filename)
+    return zf.filename
 
 
 def copy_archive(zipfile_name):
-    print('---------------\n'
-          'Copy "%s"' % zipfile_name)
-    copy_name = zipfile_name[:-4] + "(copy)" + zipfile_name[-4:]
-    zf = zipfile.ZipFile(copy_name, mode='x')
-    try:
-        zf.write('random_text.txt', arcname='random_text_compressed.txt',
-                 compress_type=compression)
-    finally:
-        zf.close()
+    print('---------------\nCopy "%s"' % zipfile_name)
 
-print()
-print_info('zipfile_append.zip')
+    zf_copy_source = zipfile.ZipFile(zipfile_name, mode='r')
+
+    # copy_name = zipfile_name[:-4] + "(copy)" + zipfile_name[-4:]
+    # zf_copy_to = zipfile.ZipFile(copy_name, mode='x')
+
+    with tempfile.TemporaryDirectory(suffix='_zip') as tmpdirname:
+        zf_copy_source.extractall(
+            tmpdirname,
+            zf_copy_source.namelist()
+        )
+        input("Extraction done! look at it!\n%s" % os.listdir(tmpdirname))
+
+if __name__ == "__main__":
+    name_to_copy = create_archive()
+    copy_archive(name_to_copy)
